416d3b7a4cbc7a0b54fadbeeb7652482d6636b8d
Jebb Burditt <jebb.burditt@gmail.com>
2025-02-23 18:47:28 -0800

    comment out partial change

--- app/DynamicsAdapter/DynamicsAdapter.Web/DynamicsAdapter.Web.SearchApiClient.partial.cs
+++ app/DynamicsAdapter/DynamicsAdapter.Web/DynamicsAdapter.Web.SearchApiClient.partial.cs
@@ -1,8 +1,8 @@
 ﻿namespace DynamicsAdapter.Web
 {
-    public partial class TaxIncomeInformation
-    {
-        [Newtonsoft.Json.JsonProperty("taxCode", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-        public TaxCode TaxCode { get; set; }
-    }
+    //public partial class TaxIncomeInformation
+    //{
+    //    [Newtonsoft.Json.JsonProperty("taxCode", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+    //    public TaxCode TaxCode { get; set; }
+    //}
 }
