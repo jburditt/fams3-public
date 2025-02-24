3e55bde52aaf55588232d5746057bc0357286e89
Jebb Burditt <jebb.burditt@gmail.com>
2025-02-23 18:39:03 -0800

    revert nuget package version and add partial for TaxCode

---
+++ app/DynamicsAdapter/DynamicsAdapter.Web/DynamicsAdapter.Web.SearchApiClient.partial.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamicsAdapter.Web
+{
+    public partial class TaxIncomeInformation
+    {
+        [Newtonsoft.Json.JsonProperty("taxCode", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public TaxCode TaxCode { get; set; }
+    }
+}
